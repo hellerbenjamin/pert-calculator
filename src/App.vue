0f27f498c80cd21bcb54b52400573fe415228d2b
Benjamin Heller <ben@benjaminheller.net>
2018-09-27 15:56:06 -0800

    calculation and validations

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@
         <label>
             Optimistic
             <input type="number" name="optimistic"
-               v-model.number="inputs.optimistic"
+                   v-model.number="inputs.optimistic"
+                   v-on:change="updateTotal"
             />
         </label>
 
         <label>
             Typical
             <input type="number" name="typical"
-               v-model.number="inputs.typical"
+                   v-model.number="inputs.typical"
+                   v-on:change="updateTotal"
             />
 
         </label>
@@ -19,12 +21,15 @@
         <label>
             Pessimistic
             <input type="number" name="pessimistic"
-               v-model.number="inputs.pessimistic"
+                   v-model.number="inputs.pessimistic"
+                   v-on:change="updateTotal"
             />
         </label>
 
-        <div id="result">
-
+        <div id="result" v-bind:class="this.totalValid">
+            Pert Total
+            <div class="warning">Please enter estimates to receive a total</div>
+            <div class="total"></div>
         </div>
     </div>
 
@@ -43,17 +48,47 @@
                     'typical': 0,
                     'pessimistic': 0
                 },
+
+                inputsValid: false,
+                total: 0,
+                totalValid: false
+
             }
         },
 
-        mounted() {
-
-        },
-
         methods: {
+            updateTotal() {
+                this.validateInputs();
 
+                let total = (this.inputs.optimistic + 4 * this.inputs.typical + this.inputs.pessimistic) / 6
 
+                if (typeof total === 'number' && this.inputsValid === true) {
+                    this.total = total;
+                    this.totalValid = true;
+                }
+            },
 
+            validateInputs() {
+                let inputs = this.inputs;
+
+                for (let key in inputs) {
+                    if (inputs[key] === 0 || typeof inputs[key] !== 'number' || this.isAbsolute( inputs[key]) === false) {
+                        this.inputsValid = false;
+                        return;
+                    }
+                }
+
+                this.inputsValid = true;
+            },
+
+            isAbsolute(input) {
+                if (typeof input !== 'number') {
+                    return false;
+                }
+
+                let abs = Math.abs(input);
+                return (abs === input);
+            }
         }
 
 
@@ -61,4 +96,5 @@
 </script>
 
 <style>
+
 </style>
