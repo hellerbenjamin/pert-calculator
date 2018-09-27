ff15b5cd0fd2f06dfceffed8acdd59d06d8e8314
Benjamin Heller <ben@benjaminheller.net>
2018-09-27 14:51:36 -0800

    vue init

---
+++ plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Pert Calculator
+ *
+ * @package     pert-calculator
+ * @author      Benjamin Heller
+ * @copyright   2018
+ * @license     GPL-2.0+
+ *
+ * @pert-calculator
+ * Plugin Name: Pert Calculator
+ * Description: A pert calculator made with VUE js
+ * Version:     1.0.0
+ * Author:      Benjamin Heller
+ * Author URI:  https://benjaminheller.net
+ * Text Domain: pert-calculator
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+
